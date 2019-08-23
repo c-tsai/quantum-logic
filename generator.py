2678dec096946c1712e45b893b82f990db6f7c49
c-tsai <43466981+c-tsai@users.noreply.github.com>
2019-08-23 11:05:24 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -15,19 +15,19 @@ for i in range(2**bit_len):
    Dict[str(i)] = [-1]
    Dict_0[str(i)] = []
 #print(Dict)
+count = 0
 df = pd.DataFrame(Dict)
 for i in range(2**bit_len):
-    print(i)
     temp = pd.DataFrame(Dict_0)
+    print(str(i)+'----------------')
     for j in range(2**bit_len):
-        for idx, row in df.iterrows():
-            row_d = {}
-            for k in range(2**bit_len):
-                row_d[str(k)]= [row[str(k)]]
-            row_d[str(j)]= i
+        row= df.copy()
+        row[str(j)] = i
+            #count += 1
+        print(temp.shape)
                 #print(type(temp), type(row_d))
-            temp = pd.concat([temp, pd.DataFrame(row_d)], ignore_index= True)
+        temp = pd.concat([temp, row], ignore_index= True)
     df= pd.concat([df, temp], ignore_index=True)
-    df.drop_duplicates()
+    df = df.drop_duplicates()
 df.to_csv('exhaustive_partial.csv', index= False)
 print(df.shape[0])
