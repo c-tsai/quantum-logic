d026b8f5affcf82fbdf2bdb9ef9c7a73f9037f92
c-tsai <43466981+c-tsai@users.noreply.github.com>
2019-08-23 17:59:55 +0800

    Update experimant.py

--- experimant.py
+++ experimant.py
@@ -9,21 +9,28 @@ import pandas as pd
 from synthesizer import QCSynthesizer
 bit_len=3
 
-df = pd.read_csv('exhaustive.csv')
-result= np.zeros((20)).astype(int)
+df = pd.read_csv('exhaustive_partial.csv')
+result= np.zeros((20,9)).astype(int)
 for idx, row in df.iterrows():
+    x=8
+    for i in row:
+        if i == -1: x -= 1 
     print(idx)
     q= QCSynthesizer(np.array(row).astype(int), bit_len)
-    q.Dym_Algorithm(permute=True, control_min=True, direction= 'bi')
+    q.DFS_Algorithm(permute=False, control_min=True, direction= 'bi')
     qc= q.output_circuit()
-    result[len(qc)] = result[len(qc)] + 1
-    for i in range(len(row)):
-        if not row[i]== qc.inf(i):
-            raise ValueError
-count, s= 0, 0
+    result[len(qc), x] = result[len(qc), x] + 1
+#    for i in range(len(row)):
+#        if not row[i]== qc.inf(i):
+#            raise ValueError('wrong result')
+res, count, s= np.sum(result, axis=1), np.zeros((9)), np.zeros((9))
 for i in range(result.shape[0]):
-    print(result[i])
-    count += i*result[i]
-    s += result[i]
-print('\n'+str(count/s))
+    print(res[i])
+    for j in range(9):
+        count[j] += i*result[i, j]
+        s[j] += result[i, j]
+print('\n')
+r = np.divide(count, s)
+for i in r:
+    print(i)
     
