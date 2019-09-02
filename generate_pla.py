daf22be2618278f1e62abe49cce9fe33f26dc555
c-tsai <43466981+c-tsai@users.noreply.github.com>
2019-09-02 15:15:23 +0800

    Update generate_pla.py

--- generate_pla.py
+++ generate_pla.py
@@ -4,23 +4,46 @@ Created on Thu Aug 22 15:15:31 2019
 
 @author: v-catsai
 """
-import pandas as pd
+import csv
 import os 
+import numpy as np
+
 bit_len=3
 name ='exhaustive'
 mode =6
 
-df = pd.read_csv(name+'.csv')
-#os.makedirs(name)
-for idx, row in df.iterrows():
+file = open(name+'.csv', 'r')
+my_reader = csv.reader(file, delimiter= ',')
+result= np.zeros((20,9)).astype(int)
+idx=0
+for row in my_reader:
+    x=8
+    for i in row:
+        if i == -1: x -= 1 
     path = 'temp.pla'
     print(idx)
+    idx += 1
     f = open(path, 'w+')
     f.write(str(bit_len)+' 2\n')
     for i in range(len(row)):
         if row[i] != -1:
-            f.write('{0:03b}'.format(i)+ ' '+ '{0:03b}'.format(int(row[i]))+'\n')
+            f.write('{0:03b}'.format(i)+ ' '+ '{0:03b}'.format(int(float(row[i])))+'\n')
     f.close()
-    os.system('./a.out temp.pla '+str(mode))
+    #string = os.popen('./a.out temp.pla '+str(mode)).read().split()
+    #result[string[-2], x] = result[string[-2], x] + 1
+    
+res, count, s= np.sum(result, axis=1), np.zeros((9)), np.zeros((9))
+for i in range(result.shape[0]):
+    print(res[i])
+    for j in range(9):
+        count[j] += i*result[i, j]
+        s[j] += result[i, j]
+print('\n')
+r = np.divide(count, s)
+for i in r:
+    print(i)
+
+print('\n')
+print(sum(count)/sum(s))
 
     
