1cf22d2760737dce7135f200110fa09cc3819616
c-tsai <c.tsai.ooxx@gmail.com>
2019-11-01 18:00:34 +0800

    Update experimant.py

--- experimant.py
+++ experimant.py
@@ -20,11 +20,12 @@ for idx, row in df.iterrows():
         if i == -1: x -= 1 
     print(idx)
     q= QCSynthesizer(np.array(row).astype(int), bit_len)
-    q.Dym_Algorithm(permute=False, control_min=True, direction= 'bi', cost_typ='NCV-012')
+    q.BFS_Algorithm(permute=False, control_min=False, direction= 'bi', cost_typ='NCV-155')
     qc= q.output_circuit()
     result111[len(qc), x] = result111[len(qc), x] + 1
-    result155[qc.cost(0, 'NCV-155'),x] = result155[qc.cost(0, 'NCV-155'),x]
-    result012[qc.cost(0, 'NCV-012'),x] = result012[qc.cost(0, 'NCV-012'),x]
+    #print(qc.cost(0, 'NCV-155'))
+    result155[qc.cost(0, 'NCV-155'),x] = result155[qc.cost(0, 'NCV-155'),x] + 1
+    result012[qc.cost(0, 'NCV-012'),x] = result012[qc.cost(0, 'NCV-012'),x] + 1
     for i in range(len(row)):
         if not row[i]== qc.inf(i) and not row[i]== -1:
             raise ValueError('wrong result '+ str(row[i])+ ' to '+ str(qc.inf(i)))
@@ -32,7 +33,7 @@ for idx, row in df.iterrows():
 
 
 
-f= open('result_syns2_control_Dym.txt', 'w+')
+f= open('result_NCV155_Dym.txt', 'w+')
 f.write('----gate cost ----'+ '\n')
 print('----gate cost ----')
 res, count, s= np.sum(result111, axis=1), np.zeros((9)), np.zeros((9))
