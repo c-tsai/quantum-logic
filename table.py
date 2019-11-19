e977b7d16597ad77b5f58bae32b7c7c6cbc129fd
c-tsai <c.tsai.ooxx@gmail.com>
2019-11-19 16:15:14 +0800

    Update table.py

--- table.py
+++ table.py
@@ -5,12 +5,19 @@ Created on Tue Nov 19 15:35:08 2019
 @author: v-catsai
 """
 class Table:
-    def __init__(self):
+    def __init__(self, length):
         self.dict = {}
+        self.length = length
+        
     def __getitem__(self, key):
         if key in self.dict: return self.dict[key]
-        else: return -1
+        elif key < self.length: return -1
+        else: 
+            raise ValueError('"'+str(key)+'" out of range (>'+str(self.length)+')')
+            
     def __setitem__(self, key, value):
         self.dict[key]= value
     def __del__(self):
         del self.dict
+        del self.length
+    def __len__(self): return self.length
