ef10869d71a2b945f52ca80075c00840021551d6
c-tsai <c.tsai.ooxx@gmail.com>
2019-11-22 13:31:43 +0800

    Update testing_spec.py

--- testing_spec.py
+++ testing_spec.py
@@ -8,21 +8,23 @@ import os
 from synthesizer import QCSynthesizer, pla_reader
 
 folder ='revlib_spec'
-f= open('BFS_NCV111_cont_revlib_result.txt')
+f= open('BFS_NCV111_cont_revlib_result.txt', 'w+')
 f.write('q_cost,gate_len,NCV-111,NCV-012,NCV-155'+'\n')
 
 for spec in os.listdir(folder):
     f.write(spec)
     print(spec)
     array, bit_len = pla_reader( os.path.join(folder, spec))
+    #print(array.shape, bit_len)
     Q= QCSynthesizer(array, bit_len)
-    Q.BFS_Algorithm(permute=False, control_min=True, direction= 'bi', cost_typ='NCV-111')
+    Q.BFS_Algorithm(permute=False, control_min=False, direction= 'bi', cost_typ='NCV-111')
     QC= Q.output_circuit()
     #print string
-    for i in range(len(array)):
+    for i in range(array.shape[0]):
         if not array[i]== QC.inf(i) and not array[i]== -1:
             raise ValueError('wrong result '+ str(array[i])+ ' to '+ str(array.inf(i)))
     #print string[-4]
+    print(QC)
     f.write(','+str(QC.cost(0,'length')))
     f.write(','+str(QC.cost(0,'NCV-111')))
     f.write(','+str(QC.cost(0,'NCV-012')))
