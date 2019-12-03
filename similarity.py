af20f3ac6e98c43c1632f55ee9e6aea3102dd40c
c-tsai <c.tsai.ooxx@gmail.com>
2019-12-03 17:21:57 +0800

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -4,35 +4,27 @@ Created on Thu Aug 22 15:15:31 2019
 
 @author: v-catsai
 """
-import os 
-from synthesizer import QCSynthesizer, pla_reader
+import numpy as np
+import pandas as pd
+from synthesizer import QCSynthesizer
+bit_len=3
 
-folder ='revlib_spec'
-f= open('BFS_NCV111_cont_revlib_result.txt', 'w+')
-f.write('q_cost,gate_len,NCV-111,NCV-012,NCV-155'+'\n')
+df = pd.read_csv('exhaustive_partial.csv')
+control_min, cost_typ= False, 'NCV-111'
+BFS_si, DFS_si= 0, 0
 
-for spec in os.listdir(folder):
-    f.write(spec)
-    print(spec)
-    array, bit_len = pla_reader( os.path.join(folder, spec))
-    #print(array.shape, bit_len)
-    Q= QCSynthesizer(array, bit_len)
-    Q.BFS_Algorithm(permute=False, control_min=False, direction= 'bi', cost_typ='NCV-111')
-    QC= Q.output_circuit()
-    #print string
-    for i in range(array.shape[0]):
-        if not array[i]== QC.inf(i) and not array[i]== -1:
-            raise ValueError('wrong result '+ str(array[i])+ ' to '+ str(array.inf(i)))
-    #print string[-4]
-    print(QC)
-    f.write(','+str(QC.cost(0,'length')))
-    f.write(','+str(QC.cost(0,'NCV-111')))
-    f.write(','+str(QC.cost(0,'NCV-012')))
-    f.write(','+str(QC.cost(0,'NCV-155')))
-    print(QC.cost(0,'length'), QC.cost(0,'NCV-111'), QC.cost(0,'NCV-012'), QC.cost(0,'NCV-155'))
-    f.write('\n')
-    
-f.close()
+for idx, row in df.iterrows():
+    print(idx)
+    q= QCSynthesizer(np.array(row).astype(int), bit_len)
+    q.Dym_Algorithm(permute=False, control_min=control_min, direction='bi', cost_typ=cost_typ)
+ 
+    qDFS= QCSynthesizer(np.array(row).astype(int), bit_len)
+    qDFS.DFS_Algorithm(permute=False, control_min=control_min, direction= 'bi', cost_typ=cost_typ)
+    DFS_si += qDFS.order_difference(q)
+   
+    qBFS= QCSynthesizer(np.array(row).astype(int), bit_len)
+    qBFS.BFS_Algorithm(permute=False, control_min=control_min, direction= 'bi', cost_typ=cost_typ)
+    BFS_si += qBFS.order_difference(q)
 
-    
-
+print('TOL DFS difference to Dym: ', DFS_si)
+print('TOL BFS difference to Dym: ', BFS_si)
