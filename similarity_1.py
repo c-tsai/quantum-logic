6a7ea9860cf31a343997071160557d865cacd78b
c-tsai <c.tsai.ooxx@gmail.com>
2019-12-05 17:19:53 +0800

    Update similarity_1.py

--- similarity_1.py
+++ similarity_1.py
@@ -10,17 +10,17 @@ from synthesizer import QCSynthesizer
 bit_len=3
 
 df = pd.read_csv('exhaustive_partial.csv')
-control_min, cost_typ= False, 'NCV-155'
+control_min, cost_typ= False, 'length'
 BFS, DFS= 0, 0
 
 for idx, row in df.iterrows():
     print(idx)
     q= QCSynthesizer(np.array(row).astype(int), bit_len)
     q.Dym_Algorithm(permute=False, control_min=control_min, direction='bi', cost_typ=cost_typ)
-    if (q.order[0]|q.order[1])==q.order[2]: DFS += 1
+    if (q.order[1]|q.order[2])==q.order[3]: DFS += 1
     else: BFS += 1
     
 
 print('amount of Dym became DFS: ', DFS)
 print('amount of Dym became BFS: ', BFS)
-print('for NCV-155 cost control_min=False')
+print('for length cost control_min=False')
