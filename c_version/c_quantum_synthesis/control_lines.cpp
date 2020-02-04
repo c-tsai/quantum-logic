9a77481be0e3fb84f15daad279464dc8aae92fee
c-tsai <c.tsai.ooxx@gmail.com>
2020-02-04 18:27:26 +0800

    finish c version of control_lines' add, pop, union function

--- c_version/c_quantum_synthesis/control_lines.cpp
+++ c_version/c_quantum_synthesis/control_lines.cpp
@@ -1,16 +1,61 @@
 #include "control_lines.h"
+#include "table.h"
+#include <algorithm>
+#include <iterator>
 
 
-void Control_lines::pop(int line, int b_num = -1) {}
-void Control_lines::unioning(Control_lines* n, int b_num ) {}
-void Control_lines::add(int line, int b_num) {}
-void Control_lines::contain(int line, int b_num) {}
-void Control_lines::copy() {}
+void Control_lines::pop(int line, int b_num) {
+	int b = (b_num == -1) ? Hamming_Dist(line, 0, bit_len) : b_num;
+	auto got = lib[b]->find(line);
+	if (got != lib[b]->end()) { lib[b]->erase(got); }
+	if (b == mn && lib[b]->empty()) { 
+		for (int i = (mn + 1); i <= mx; i++) {
+			if (!(lib[i]->empty())) { mn = i; break; }}}
+	else if (b == mx && lib[b]->empty()) {
+		for (int i = (mx -1); i >= mn; i--) {
+			if (!(lib[i]->empty())) { mx = i; break; }
+		}
+	}
+}
+void Control_lines::unioning(Control_lines* other, int b_num ) {
+	if (b_num != -1) {
+		std::set<int>* neww = new std::set<int>; int b = b_num;
+		std::set_union(lib[b]->begin(), lib[b]->end(), 
+			other->get_group(b)->begin(), other->get_group(b)->end(), std::inserter(*neww,neww->end()));
+		if (b > mx) { mx = b; }
+		if (b < mn) { mn = b; }
+		delete lib[b]; lib[b] = neww;
+	}
+	else {
+		for (int i = other->mn; i <= other->mx; i++) {
+			std::set<int>* neww = new std::set<int>;
+			std::set_union(lib[i]->begin(), lib[i]->end(),
+				other->get_group(i)->begin(), other->get_group(i)->end(), std::inserter(*neww,neww->end()));
+			delete lib[i]; lib[i] = neww;
+		}
+		if (other->mn < mn) { mn = other->mn; }
+		if (other->mx > mx) { mx = other->mx; }
+	}delete other;
+}
+void Control_lines::add(int line, int b_num) {
+	int b = (b_num == -1) ? Hamming_Dist(line, 0, bit_len) : b_num;
+	//for (auto i = lib[b]->begin(); i != lib[b]->end(); i++) { std::cout << *i; }//for testing
+	//std::cout << "if the set empty " << lib[b]->empty() << std::endl;
+	lib[b]->insert(line);
+	//std::cout << "inserted" << std::endl;
+	if (lib[mn]->empty()) { mn = b; }
+	if (lib[mx]->empty()) { mx = b; }
+	if (b > mx) { mx = b; }
+	if (b < mn) { mn = b; }
+	//std::cout << line << " added, mx= " << mx << " mn= " << mn << std::endl;
+}
+bool Control_lines::contain(int line, int b_num) {}
+Control_lines* Control_lines::copy() {}
 
 void Control_lib::pop(int line, int b_num) {}
 void Control_lib::add(int line, int b_num) {}
 int Control_lib::get(int targ, int b_num ) {}
 
-void Control_generator::remove(int targ);
-void Control_generator::combine(std::unordered_set<int>* b_list, int targ, int num);
-std::unordered_set<int>* Control_generator::best_clines(int bit1, int controled);
+void Control_generator::remove(int targ) {}
+void Control_generator::combine(std::set<int>* b_list, int targ, int num) {}
+std::set<int>* Control_generator::best_clines(int bit1, int controled) {}
--- c_version/c_quantum_synthesis/control_lines.h
+++ c_version/c_quantum_synthesis/control_lines.h
@@ -1,49 +1,79 @@
 #include <unordered_map>
-#include <unordered_set>
+#include <set>
+#include <iostream>// for testing
 
 
 class Control_lines {
 public:
 	Control_lines(int len) {
 		bit_len = len;
-		lib = new std::unordered_set<int>*[len+1];
-		for (int i = 0; i < len; i++) { lib[i] = new std::unordered_set<int>; }
+		lib = new std::set<int>*[len+1];
+		for (int i = 0; i <= len; i++) { lib[i] = new std::set<int>; }
 		mn = 0; mx = 0; 
 	}
 	~Control_lines() { 
-		for (int i = 0; i < bit_len; i++) {delete lib[i]}
+		for (int i = 0; i < bit_len; i++) { delete lib[i]; }
 		delete[] lib; }
 	friend class Control_generator;
 
 
-	class iterator: std::unordered_set<int>::iterator {
+	class iterator{
 	public:
-		iterator(std::unordered_set<int>::iterator i, int m, int id): std::unordered_set<int>::iterator(i){
-			iter = i; mx = m; iterid = id;
-		}
-		iterator& operator ++ ();
-		iterator operator ++ (int);
-	private:
-		int mx;
+		iterator(const std::set<int>::iterator& i, Control_lines* c, int id){
+			iter = i; iterid = id; origin = c; mx = c->mx;
+			end = c->get_group(id)->end();}
+		iterator(int id) { iterid = id; }
+		iterator operator ++ (int) {
+			//std::cout << "increment" << std::endl;
+			if (!(origin->get_group(iterid)->empty())) { iter++; }
+			while (iter == end && iterid < mx) {
+				//std::cout << "next strand" << std::endl;
+				iterid++; iter = origin->get_group(iterid)->begin(); end = origin->get_group(iterid)->end();
+			}
+			return *this;
+		};
+		bool operator == (const iterator& other) { 
+			return ((iter == other.iter)&&(origin==other.origin)&&(iterid==other.iterid)); }
+		bool operator != (const iterator& other) {
+			//std::cout << "comparing iterid: " << iterid << " origin: "<< origin <<  std::endl;
+			//std::cout << "and iterid: " << other.iterid << " origin: " << other.origin << std::endl;
+			return ((iter != other.iter) || (origin != other.origin) || (iterid != other.iterid)); }
+		int element() {
+			//std::cout << *iter << std::endl;
+			return *iter; }
+		virtual int first() { return 0; }
+		virtual Control_lines* second() { return 0; }
+	protected:
+		Control_lines* origin;
+		std::set<int>::iterator iter;
+		std::set<int>::iterator end;
 		int iterid;
+		int mx;
 	};
-	iterator begin() { iterid = mn; return iterator(lib[mn]->begin(),mx,mn); }
-	iterator end() { return iterator(lib[mx]->end(),mx,mx); }
+	iterator begin() {
+		//std::cout << "begin created " << std::endl;
+		//std::unordered_set<int>::iterator i = lib[mn]->begin(); // for testing
+		return iterator(lib[mn]->begin(),this,mn); }
+	iterator end() {
+		//std::cout << "end created " << std::endl;
+		return iterator(lib[mx]->end(),this,mx); }
 
 
 
 	void pop(int line, int b_num = -1);
-	void unioning(Control_lines* n, int b_num = -1);
-	std::unordered_set<int>* max_group() { return lib[mx]; };
-	std::unordered_set<int>* min_group() { return lib[mn]; };
+	void unioning(Control_lines* other, int b_num = -1);
+	void add(int line, int b_num = -1);// for testing put in public
+	std::set<int>* max_group() { return lib[mx]; };
+	std::set<int>* min_group() { return lib[mn]; };
+	std::set<int>* get_group(int key) { return lib[key]; };
+	int get_max() { return mx; }
 
 private:
-	void add(int line, int b_num = -1);
+	//void add(int line, int b_num = -1); the real private add
 	bool contain(int targ, int b_num = -1);
-	std::unordered_set<int>* get_group(int key) { return lib[key]; };
 	Control_lines* copy();
 	
-	std::unordered_set<int>** lib;
+	std::set<int>** lib;
 	int bit_len;
 	int mn;
 	int mx;
@@ -52,29 +82,28 @@ private:
 
 
 
-class Control_lib {
+class Control_lib: public Control_lines{
 public:
-	Control_lib(int len) {
-		bit_len = len;
+	Control_lib(int len): Control_lines(len) {
 		lib = new std::unordered_map<int, Control_lines*>* [len + 1];
-		for (int i = 0; i < len; i++) { 
-			lib[i] = new std::unordered_map<int, Control_lines*>
+		for (int i = 0; i <= len; i++) { 
+			lib[i] = new std::unordered_map<int, Control_lines*>;
 		}
-		mn = 0; mx = 0; iterid = -1;
 	}
+	class iterator : public Control_lines::iterator {
+	public:
+		iterator(const std::unordered_map<int, Control_lines*>::iterator& i, Control_lib* c, int id):Control_lines::iterator(id){
+			iter = i;  origin = c; mx = c->get_max();
+			end = c->get_group(id)->end();
+		}
+		int first() { return iter->first; }
+		Control_lines* second() { return iter->second; }
+	private:
+		std::unordered_map<int, Control_lines*>::iterator iter;
+	};
 	friend class Control_generator;
 
-	class iterator : std::unordered_map<int, Control_lines*>::iterator {
-	public:
-		iterator(std::unordered_map<int, Control_lines*>::iterator i, int m, int id) : std::unordered_map<int, Control_lines*>::iterator(i) {
-			iter = i; mx = m; iterid = id;
-		}
-		iterator& operator ++ ();
-		iterator operator ++ (int);
-	private:
-		int mx;
-		int iterid;
-	};
+	
 	void pop(int line, int b_num = -1);
 
 private:
@@ -98,8 +127,8 @@ public:
 	}
 	~Control_generator() { delete lib, unable; delete[]  each_num; }
 	void remove(int targ);
-	void combine(std::unordered_set<int>* b_list, int targ, int num);
-	std::unordered_set<int>* best_clines(int bit1, int controled);
+	void combine(std::set<int>* b_list, int targ, int num);
+	std::set<int>* best_clines(int bit1, int controled);
 
 private:
 	Control_lines* unable;
@@ -109,3 +138,18 @@ private:
 	int allowed_num;
 	int bit_len;
 };
+
+int main(int argc, char** argv) {
+	Control_lines* c = new Control_lines(3);
+	Control_lines* d = new Control_lines(3);
+	d->add(0);
+	c->add(1);
+	//std::cout << "before add 7" << std::endl;
+	d->add(4);
+	c->add(7);
+	c->unioning(d);
+	for (auto i = c->begin(); i != c->end(); i++) {
+		std::cout << i.element() << std::endl;
+	}
+	std::cout <<"max group: " << c->get_max() << std::endl;
+}
